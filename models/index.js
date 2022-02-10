9dc83e9a9f21ed1742c61bd928aa1c40325fe7ff
Chidori <business.faissal@outlook.com>
2022-02-10 14:52:54 -0600

    got the index folder imports and exports setup prior to writing the associations

---
+++ models/index.js
@@ -0,0 +1,25 @@
+import QaSuper from `./QaSupervisor`;
+import QaAgent from `./QaAgent`;
+import EscaUnitSuper from `./EscaUnitSuper`
+import EscaUnit from `./EscaUnit`;
+import CsrSuper from `./CsrSuper`;
+import Csr from `./Csr`;
+
+
+
+
+
+
+
+
+
+
+
+export {
+  QaSuper,
+  QaAgent,
+  EscaUnitSuper,
+  EscaUnit,
+  CsrSuper,
+  Csr
+}
