fb691a79fb6db4f4ef8dab1e9f4aa3c9da19af52
Chidori <business.faissal@outlook.com>
2022-02-10 15:21:32 -0600

    csr model draft created. Will implement date datatype next.

--- models/Csr.js
+++ models/Csr.js
@@ -1,2 +1,33 @@
-import { Model, DataTypes } from `sequelize`;
+const { Model, DataTypes } = require(`sequelize`);
 const sequelize = require(`../config/connection`);
+
+class Csr extends Model {}
+
+Csr.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  employee_number: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isNumeric: true,
+    },
+  },
+  site: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  start_date: {},
+});
