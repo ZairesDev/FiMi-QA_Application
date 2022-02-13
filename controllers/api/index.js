24ac2dcd1c421f5205b7e920f0b1d19e2cd1f2bb
MichValenz <michvalenz27@gmail.com>
2022-02-12 22:46:56 -0600

    added eu reference in index file of api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const csrRoutes = require("./callspecialist-routes.js");
+const csrRoutes = require("./callspecialist-routes");
+const euRoutes = require("./escalationUnit-routes");
 
 router.use("/csr", csrRoutes);
+router.use("/eu", euRoutes);
 
 module.exports = router;
