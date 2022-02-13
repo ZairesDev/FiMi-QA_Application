170af88e701e55e5676254798028b98e3a548b85
MichValenz <michvalenz27@gmail.com>
2022-02-12 23:35:06 -0600

    added post and comment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const csrRoutes = require("./callspecialist-routes");
 const euRoutes = require("./escalationUnit-routes");
+const qaRoutes = require("./qaAgent-routes");
 
 router.use("/csr", csrRoutes);
 router.use("/eu", euRoutes);
+router.use("/qa", qaRoutes);
 
 module.exports = router;
