fd615d07b5ca26c3c7db35cd25409a4b06f04f50
MichValenz <michvalenz27@gmail.com>
2022-02-13 22:57:35 -0600

    fixed route for log out and login

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,5 +1,5 @@
 async function logout() {
-  const response = await fetch("/api/employees/logout", {
+  const response = await fetch("/api/qaagents/logout", {
     method: "post",
     headers: { "Content-Type": "application/json" },
   });
