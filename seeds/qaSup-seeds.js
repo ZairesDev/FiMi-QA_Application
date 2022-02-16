a6f802a8c8692f9c44d0239cfcf374203130fc55
MichValenz <michvalenz27@gmail.com>
2022-02-15 20:51:53 -0600

    added seeds

--- seeds/qaSup-seeds.js
+++ seeds/qaSup-seeds.js
@@ -1,18 +1,16 @@
 const { QaSuper } = require("../models");
 
 const QaSuperData = [
-    {
-      id: 1,
-      first_name: "Mike",
-      last_name: "Couch",
-      "qa_agent",
-      "language",
-      "site",
-      "username",
-      "email",
-      "password",
-
-    },
+  {
+    id: 1,
+    first_name: "Mike",
+    last_name: "Couch",
+    language: "ENG",
+    site: "San Antonio",
+    username: "couch123",
+    email: "couch123@gmail.com",
+    password: "cats123",
+  },
 ];
 
 const seedQaSupStaff = () => QaSuper.bulkCreate(QaSuperData);
