3582f640cf4b1c67427c10bf99931f45f45e8c8e
906284 <michaelcounseller@gmail.com>
2022-02-17 09:33:22 -0600

    fixed the responsiveness of the table and sidebar layout as a whole, centered the download button properly

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,50 +3,50 @@ const router = require("express").Router();
 const { Employee } = require("../models");
 
 router.get("/", (req, res) => {
-  Employee.findAll({
-    attributes: [
-      "id",
-      "first_name",
-      "last_name",
-      "employee_number",
-      "site",
-      "role",
-      "language",
-      "group",
-      "employeesuper_id",
-      "qaAgent_id",
-    ],
-  })
-    .then(res.render("dashboard", { loggedIn: req.session.loggedIn }))
+    Employee.findAll({
+            attributes: [
+                "id",
+                "first_name",
+                "last_name",
+                "employee_number",
+                "site",
+                "role",
+                "language",
+                "group",
+                "employeesuper_id",
+                "qaAgent_id",
+            ],
+        })
+        .then(res.render("spreadsheet", { loggedIn: req.session.loggedIn }))
 
     .catch((err) => {
-      res.status(500).json(err);
+        res.status(500).json(err);
     });
 });
 
 router.get("/:id", (req, res) => {
-  Employee.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: [
-      "id",
-      "first_name",
-      "last_name",
-      "employee_number",
-      "site",
-      "role",
-      "language",
-      "group",
-      "employeesuper_id",
-      "qaAgent_id",
-    ],
-  })
+    Employee.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: [
+            "id",
+            "first_name",
+            "last_name",
+            "employee_number",
+            "site",
+            "role",
+            "language",
+            "group",
+            "employeesuper_id",
+            "qaAgent_id",
+        ],
+    })
 
-    .then(res.render("dashboard", { loggedIn: req.session.loggedIn }))
+    .then(res.render("spreadsheet", { loggedIn: req.session.loggedIn }))
 
     .catch((err) => {
-      res.status(500).json(err);
+        res.status(500).json(err);
     });
 });
 
