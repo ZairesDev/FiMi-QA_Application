7612e09cc23301e27a2e1da06c55fc86e488ba32
Chidori <business.faissal@outlook.com>
2022-02-17 16:00:27 -0600

    made minor changes to spreadsheet handlebars to reflect agreed upon changes. Added the table.js code to load the information into the spreadsheet.

--- public/javascript/table.js
+++ public/javascript/table.js
@@ -1,3 +1,27 @@
 const tableFormHandler = async (event) => {
   event.preventDefault();
+
+  const response = await fetch(`/api/employees`, {
+    method: `POST`,
+    body: JSON.stringify({
+      id,
+      first_name,
+      last_name,
+      employee_number,
+      site,
+      role,
+      language,
+      group,
+      employeesuper_id,
+      qaAgent_id,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response.ok) {
+    console.log(`Success!`);
+  } else {
+    alert(response.statusText);
+  }
 };
+
+window.onload = tableFormHandler;
