e6d20d511dd19552222da73deda1d3f341e1299f
Chidori <business.faissal@outlook.com>
2022-02-16 23:55:55 -0600

    modififed and inserted add and update employee code in employee-form.js

--- controllers/employeeform-routes.js
+++ controllers/employeeform-routes.js
@@ -69,23 +69,22 @@ router.post("/", (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  Employee.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((employeeData) => {
-      if (!employeeData) {
-        res.status(404).json({ message: "No employee found" });
-        return;
-      }
-      res.json(employeeData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+router.put("/:id", async (req, res) => {
+  try {
+    const employeeData = await Employee.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
     });
+    if (!employeeData) {
+      res.status(404).json({ message: "No employee found" });
+      return;
+    }
+    res.json(employeeData);
+  } catch (error) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", (req, res) => {
--- public/javascript/employee-form.js
+++ public/javascript/employee-form.js
@@ -1,54 +1,78 @@
 const addEmployeeFormHandler = async (event) => {
   event.preventDefault();
 
-  const firstName = document.querySelector(`#firstNameInput`).value.trim();
-  const lastName = document.querySelector(`#lastNameInput`).value.trim();
-  const employeeNumber = document.querySelector(`#empNumberInput`).value.trim();
-  const site = document.querySelector(`#workSiteInput`).value.trim();
-  const role = document.querySelector(`#roleInput`).value.trim();
-  const language = document.querySelector(`#languageInput`).value.trim();
-  const group = document.querySelector(`#groupInput`).value.trim();
-  const employeeSupervisor = querySelector(`#supervisorInput`).value.trim();
-  const qaAgent = document.querySelector(`#qaAgentInput`).value.trim();
+  const first_name = document.querySelector(`input[name='first-name']`).value;
+  const last_name = document.querySelector(`input[name='last-name']`).value;
+  const employee_number = document.querySelector(`input[name='emp-number']`).value;
+  const site = document.querySelector(`input[name='work-site']`).value;
+  const role = document.querySelector(`input[name='role']`).value;
+  const language = document.querySelector(`input[name='lang']`).value;
+  const group = document.querySelector(`input[name='group']`).value;
+  const employeesuper_id = querySelector(`input[name='supervisor']`).value;
+  const qaAgent_id = document.querySelector(`input[name='qa-agent']`).value;
 
-  if (
-    firstName &&
-    lastName &&
-    employeeNumber &&
-    site &&
-    role &&
-    language &&
-    group &&
-    employeeSupervisor &&
-    qaAgent
-  ) {
-    const response = await fetch(`./form`, {
-      method: `post`,
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        employeeNumber,
-        site,
-        role,
-        language,
-        group,
-        employeeSupervisor,
-        qaAgent,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response) {
-      console.log(`success!`);
-    } else {
-      alert(response.statusText);
-    }
+  const response = await fetch(`/form`, {
+    method: `post`,
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      employee_number,
+      site,
+      role,
+      language,
+      group,
+      employeesuper_id,
+      qaAgent_id,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  if (response) {
+    document.location(`/dashboard`);
+  } else {
+    alert(response.statusText);
   }
 };
 
 const updateEmployee = async (event) => {
   event.preventDefault();
+
+  const employeeId = window.location.toString().split(`/`)[
+    window.location.toString().split(`/`).length - 1
+  ];
+
+  const first_name = document.querySelector(`input[name='first-name']`).value.trim();
+  const last_name = document.querySelector(`input[name='last-name']`).value.trim();
+  const employee_number = document.querySelector(`input[name='emp-number']`).value.trim();
+  const site = document.querySelector(`input[name='work-site']`).value.trim();
+  const role = document.querySelector(`input[name='role']`).value.trim();
+  const language = document.querySelector(`input[name='lang']`).value.trim();
+  const group = document.querySelector(`input[name='group']`).value.trim();
+  const employeesuper_id = querySelector(`input[name='supervisor']`).value.trim();
+  const qaAgent_id = document.querySelector(`input[name='qa-agent']`).value.trim();
+
+  const response = await fetch(`/form/${employeeId}`, {
+    method: `PUT`,
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      employee_number,
+      site,
+      role,
+      language,
+      group,
+      employeesuper_id,
+      qaAgent_id,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    document.location.replace(`/dashboard`);
+  } else {
+    alert(response.statusText);
+  }
 };
 
-document
-  .querySelector(`.add-button`)
-  .addEventListener(`submit`, addEmployeeFormHandler);
+document.querySelector(`.add-button`).addEventListener(`submit`, addEmployeeFormHandler);
+document.querySelector(`.update-button`).addEventListener(`submit`, updateEmployee);
