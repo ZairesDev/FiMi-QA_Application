0e5b8d30cac0866567c7cc4d12ccd00a85acf896
Chidori <business.faissal@outlook.com>
2022-02-17 16:37:26 -0600

    commented out table.js to use handlebars helpers instead.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,27 +1,25 @@
 async function loginFormHandler(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const username = document.querySelector("#logUsernameInput").value.trim();
-    const password = document.querySelector("#logPasswordInput").value.trim();
+  const username = document.querySelector('#logUsernameInput').value.trim();
+  const password = document.querySelector('#logPasswordInput').value.trim();
 
-    if (username && password) {
-        const response = await fetch('/api/qa', {
-            method: "post",
-            body: JSON.stringify({
-                username,
-                password,
-            }),
-            headers: { "Content-Type": "application/json" },
-        });
+  if (username && password) {
+    const response = await fetch('/api/qa', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-            document.location.replace("/");
-        } else {
-            alert(response.statusText);
-        }
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
+  }
 }
 
-document
-    .querySelector(".sign-form")
-    .addEventListener("submit", loginFormHandler);
+document.querySelector('.sign-form').addEventListener('submit', loginFormHandler);
