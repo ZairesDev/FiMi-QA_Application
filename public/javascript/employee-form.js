c3db2c1dd8f5c2ac82723125185458546cb3f86d
Chidori <business.faissal@outlook.com>
2022-02-16 21:43:41 -0600

    created addemployee code

--- public/javascript/employee-form.js
+++ public/javascript/employee-form.js
@@ -1,29 +1,54 @@
-const newEmployeeFormHandler = async (event) => {
+const addEmployeeFormHandler = async (event) => {
   event.preventDefault();
 
-  const qaSupervisor = document.querySelector(`#qaSupervInput`).value.trim();
-  const qaAgent = document.querySelector(`#qaAgentInput`).value.trim();
-  const employeeSupervisor = querySelector(`#supervisorInput`).value.trim();
   const firstName = document.querySelector(`#firstNameInput`).value.trim();
   const lastName = document.querySelector(`#lastNameInput`).value.trim();
-  const employeeId = document.querySelector(`#empIdInput`).value.trim();
+  const employeeNumber = document.querySelector(`#empNumberInput`).value.trim();
+  const site = document.querySelector(`#workSiteInput`).value.trim();
   const role = document.querySelector(`#roleInput`).value.trim();
-  const workSite = document.querySelector(`#workSiteInput`).value.trim();
   const language = document.querySelector(`#languageInput`).value.trim();
-  const email = document.querySelector(`#emailInput`).value.trim();
+  const group = document.querySelector(`#groupInput`).value.trim();
+  const employeeSupervisor = querySelector(`#supervisorInput`).value.trim();
+  const qaAgent = document.querySelector(`#qaAgentInput`).value.trim();
 
   if (
-    qaSupervisor &&
-    qaAgent &&
-    employeeSupervisor &&
     firstName &&
     lastName &&
-    employeeId &&
+    employeeNumber &&
+    site &&
     role &&
-    workSite &&
     language &&
-    email
+    group &&
+    employeeSupervisor &&
+    qaAgent
   ) {
-    const response = await fetch(``);
+    const response = await fetch(`./form`, {
+      method: `post`,
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        employeeNumber,
+        site,
+        role,
+        language,
+        group,
+        employeeSupervisor,
+        qaAgent,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response) {
+      console.log(`success!`);
+    } else {
+      alert(response.statusText);
+    }
   }
 };
+
+const updateEmployee = async (event) => {
+  event.preventDefault();
+};
+
+document
+  .querySelector(`.add-button`)
+  .addEventListener(`submit`, addEmployeeFormHandler);
